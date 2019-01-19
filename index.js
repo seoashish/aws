49620947f562df078212eb155e4cffebd91e9e3f
seoashish <seoashishkerketta@gmail.com>
2019-01-19 17:06:41 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(8080, function(){
-  console.log('Server listening on PORT 8080...');
+}).listen(8000, function(){
+  console.log('Server listening on PORT 8000...');
 });
